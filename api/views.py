3384c22b5b825bd20bccb9939471800b33cbbf62
pavelhardzei <pavelgordei11@gmail.com>
2021-07-12 08:45:41 +0300

    added generic class-based views

--- api/views.py
+++ api/views.py
@@ -1,62 +1,13 @@
 from .models import Article
 from .serializers import ArticleSerializer
-# from rest_framework import generics
-from rest_framework.views import APIView
-from django.http import Http404
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics
 
 
-# class ArticleList(generics.ListCreateAPIView):
-#     queryset = Article.objects.all()
-#     serializer_class = ArticleSerializer
-#
-#
-# class ArticleDetails(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Article.objects.all()
-#     serializer_class = ArticleSerializer
+class ArticleList(generics.ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
 
 
-class ArticleList(APIView):
-
-    @staticmethod
-    def get(request):
-        articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        return Response(serializer.data)
-
-    @staticmethod
-    def post(request):
-        serializer = ArticleSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ArticleDetails(APIView):
-
-    @staticmethod
-    def get_object(pk):
-        try:
-            return Article.objects.get(pk=pk)
-        except Article.DoesNotExist:
-            return Http404
-
-    def get(self, request, pk):
-        article = self.get_object(pk)
-        serializer = ArticleSerializer(article)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        article = self.get_object(pk)
-        serializer = ArticleSerializer(article, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        article = self.get_object(pk)
-        article.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class ArticleDetails(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
