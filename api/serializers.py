8dfab0fac26751e2becadbe4ba7ff3a99dd4600d
pavelhardzei <pavelgordei11@gmail.com>
2021-07-12 08:52:17 +0300

    updated

--- api/serializers.py
+++ api/serializers.py
@@ -6,17 +6,3 @@ class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ['id', 'title', 'description']
-
-
-# class ArticleSerializer(serializers.Serializer):
-#     title = serializers.CharField(max_length=100)
-#     description = serializers.CharField(max_length=1000)
-#
-#     def create(self, validated_data):
-#         return Article.objects.create(validated_data)
-#
-#     def update(self, instance, validated_data):
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.description = validated_data.get('description', instance.description)
-#         instance.save()
-#         return instance
