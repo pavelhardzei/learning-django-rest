9a6e5f3a5163630881aa824ac97d84047641ec56
pavelhardzei <pavelgordei11@gmail.com>
2021-07-14 09:13:02 +0300

    some updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from "react";
+import {ArticleList} from "./components/ArticleList";
 
 function App() {
+
+    const [articles, setArticles] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:8000/articles/', {
+            'method': 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(resp => resp.json())
+            .then(resp => setArticles(resp))
+            .catch(error => console.log(error))
+    }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React and Django</h1>
+      <ArticleList articles={articles}/>
     </div>
   );
 }
