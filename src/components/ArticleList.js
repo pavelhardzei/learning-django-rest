60aab729eb8ef0ee7e2c6f0b5f35955192517be4
pavelhardzei <pavelgordei11@gmail.com>
2021-07-14 09:21:44 +0300

    updated component

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,14 +1,29 @@
 import React from 'react';
 
 export const ArticleList = (props) => {
+
+    const editBtn = (article) => {
+        props.editBtn(article)
+    }
+
     return (
         <div>
             {props.articles && props.articles.map(article => {
                 return (
-                    <div key={props.articles.indexOf(article)}>
+                    <div key={article.id}>
                         <h2>{article.title}</h2>
                         <p>{article.description}</p>
-                        <hr/>
+
+                        <div className="row">
+                            <div className="col-md-1">
+                                <button className="btn btn-primary" onClick={() => editBtn(article)}>Update</button>
+                            </div>
+                            <div className="col">
+                                <button className="btn btn-danger">Delete</button>
+                            </div>
+                        </div>
+
+                        <hr className="hrclass"/>
                     </div>
                 )
             })}
